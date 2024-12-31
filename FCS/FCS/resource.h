0993ecc991b3cbbedb1a7224c32a965e621ececd
WolfFoox <1421963741@qq.com>
2024-12-31 18:01:26 +0800

    fifteenth commit

---
+++ FCS/FCS/resource.h
@@ -0,0 +1,38 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 FCS.rc 使用
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_FCSTYPE                     130
+#define MYDIALOG                        310
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
+#define IDC_EDIT3                       1002
+#define IDC_EDIT4                       1003
+#define IDC_RADIO1                      1004
+#define IDC_RADIO2                      1005
+#define IDC_EDIT5                       1006
+#define IDC_EDIT6                       1007
+#define IDC_EDIT7                       1008
+#define IDC_EDIT8                       1009
+#define IDC_EDIT9                       1010
+#define IDC_EDIT10                      1011
+#define IDC_EDIT11                      1012
+#define ID_32771                        32771
+#define ID_C                            32772
+#define ID_START                        32773
+#define ID_PAUSE                        32774
+#define ID_STOP                         32775
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_COMMAND_VALUE         32780
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
